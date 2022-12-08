3a4b68fc9921819330ff8e5493ea72798eaaf653
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-08 14:50:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main Entry point
- * Description: This program prints using function puts
+ * Description: 'This program prints using function puts'
  * Return: Always 0 (Success)
  **/
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
