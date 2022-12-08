d659afe18840ef14089727ee8cfd0d98f554d085
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-08 15:32:47 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main marks the entry point into the program
+ * Description: This programs prints hello world using function printf
+ * Returns zero to mark success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
