0a8624e8b7a1e4c784417100a9db35f39f5e19bd
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-08 16:43:01 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
- * main Entry point
- *
- * Description: This program prints using function puts
- *
- * Return: Always 0 (Success)
+ * main - main marks the entry point into the program
+ * @void: When parameter X is void, it means it is not accepting any args
+(*
+ * Description: Uses puts to print strings only as standard output)?
+(* stdio header: Contains function puts)*
+ * Return: returned value is 0 which means the execution was successful
  */
 int main(void)
 {
