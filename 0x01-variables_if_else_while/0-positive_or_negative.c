464e161d45cc6cb8fdf8231cb15da4a5956b2b37
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-09 07:11:58 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,22 +5,20 @@
  * main - main marks the entry point into the program
  * @void: When parameter X is void, it means it is not acceptions any arg
 (* Description: Prints a random number and states whether)?
- *	it is positive or negative or zero
  *
-(* stdlib.h header: memory allocation, process control, conversions and others)*
-(* time.h header: provide standardized access to time/date manipulation and formatting)*
+(* stdlib.h header: memory allocation, process control, conversions*
+(* time.h header: provide standardized access to time/dat)*
  * Return: Always 0.
  */
 int main(void)
 {
 	int n;
 
-	/*srand(time(0));*/
 	/* Intializes random number generator */
 	srand(time(NULL));
 	n = rand() - RAND_MAX / 2;
 
-	if (n>0)
+	if(n>0)
 		printf("%d is positive\n", n);
 	else if(n<0)
 		printf("%d is negative\n", n);
