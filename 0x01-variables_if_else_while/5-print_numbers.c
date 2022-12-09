3faf32a60ad43c7bee88f72afa5f8376c7de4158
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-09 11:25:26 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - main marks the entry point into the program
+* @void: When parameter X is void, it means it is not acceptions any arg
+(* Description: Write a program that prints all single digit numbers of...)?
+*  base 10 starting from 0, followed by a new line
+*
+(* stdio.h header: performing input and output)*
+* Return: Always 0.
+*/
+int main(void)
+{
+/*Declaration*/
+int n;
+
+/*For Loop for iterating through all single*/
+/* digit numbers of base 10 starting from 0*/
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
