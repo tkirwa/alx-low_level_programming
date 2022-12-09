10fa6b491bc3d42a6c8a2c2541be357dea892745
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-08 16:30:55 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - main marks the entry point into the program
  * @void: When parameter X is void, it means it is not acceptions any arg
 (* Description: Uses printf to print formatted text to the standard output)?
- * 
+ *
 (* stdio header: Contains function printf)*
  * Return: returned value is 0 which means the execution was successful
  */
