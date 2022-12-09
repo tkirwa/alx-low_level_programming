5c8c961f514bb52833c04bd3c80f2275130bbe16
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-08 18:03:03 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	int a;
 	long int b;
-       	long long int c;
-       	char d;
+	long long int c;
+	char d;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
