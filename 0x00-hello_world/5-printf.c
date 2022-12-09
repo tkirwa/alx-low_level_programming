480e7e039c6aee6caad54c3474290cc0ee5eeb8f
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-08 16:26:56 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- *
  * main - main marks the entry point into the program
- *
- * Description: This programs prints hello world using function printf
- (* stdio.h: it has printf)*
- *
- * Return: Returns zero to mark success
- *
+ * @void: When parameter X is void, it means it is not acceptions any arg
+(* Description: Uses printf to print formatted text to the standard output)?
+ * 
+(* stdio header: Contains function printf)*
+ * Return: returned value is 0 which means the execution was successful
  */
 int main(void)
 {
