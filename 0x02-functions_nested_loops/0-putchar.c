3da5537b9c96cd3b24f1ec22cbca1d844a13fbf6
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-14 08:42:25 -0800

    0-putchar.c - Write a program that prints _putchar, followed by a new line. - The program should return 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* main - main marks the entry point into the program
+* @void: When parameter X is void, it means it is not acceptions any arg
+(* Description: program that prints _putchar, followed by a new line....)?
+*
+(* main.h header: performing input and output)*
+* Return: Always 0.
+*/
+int main(void)
+{
+    /*Prints _putchar followed by a newline*/
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    /*Return 0*/
+    return (0);
+}
