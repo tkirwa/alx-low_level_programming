166d67f07aeadba70f86e5fd0493c382af0deb1f
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-14 11:07:32 -0800

    4-isalpha.c - Write a function that checks for alphabetic character. - Prototype: int _isalpha(int c); - Returns 1 if c is a letter, lowercase or uppercase - Returns 0 otherwise

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,11 @@
  * _isalpha - tests whether a character is a
  * alphabetic character.
  * @c: character to test.
- * Return: 1 if it is a alphabetic character, 0 if it is not a alphabetic character.
+ * Return: 1 if it is a alphabetic character...
+ * 0 if it is not a alphabetic character.
  */
-int _isalpha(int c) {
-return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+
