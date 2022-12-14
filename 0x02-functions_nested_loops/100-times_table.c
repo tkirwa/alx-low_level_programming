542d4c90a6414f6c5699998f64f55cf3f4e1d2a8
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-14 15:03:43 -0800

    100-times_table.c - Write a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,21 +21,24 @@ int prod = j * i;
 if (j == 0)
 {
 _putchar('0');
-} else if (prod <= 9)
+}
+else if (prod <= 9)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar('0' + prod);
-} else if (prod > 99)
+}
+else if (prod > 99)
 {
 _putchar(',');
 _putchar(' ');
 _putchar('0' + (prod / 100));
 _putchar('0' + ((prod / 10) % 10));
 _putchar('0' + (prod % 10));
-} else
+}
+else
 {
 _putchar(',');
 _putchar(' ');
