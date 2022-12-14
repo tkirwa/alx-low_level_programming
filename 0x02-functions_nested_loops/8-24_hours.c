a2336b75f4669e2d7ae5f1084bb76358256cb76a
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-14 13:51:42 -0800

    8-24_hours.c - Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59. - Prototype: void jack_bauer(void);

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - function that prints every minute of the day, 24 hour clock
+ * h = hour, m = minutes
+ * / 10 allows second digit to rotate
+ * for loop breaks before passing 24:00
+ * Return: 24 hour clock line by line
+ */
+void jack_bauer(void)
+{
+int i;
+int j;
+
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar('\n');
+}
+}
+}
