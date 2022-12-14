a4d89e998c7ca980628d71b20a034084b69bc915
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-14 08:49:48 -0800

    0-putchar.c - Write a program that prints _putchar, followed by a new line. - The program should return 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,16 +9,16 @@
 */
 int main(void)
 {
-    /*Prints _putchar followed by a newline*/
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    /*Return 0*/
-    return (0);
+/* Prints _putchar followed by a newline */
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+/* Return 0 */
+return (0);
 }
