e95fa5190c3abc56520d8a4ac31f694871ef620e
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-15 18:37:20 -0800

    972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,11 +10,11 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
@@ -22,5 +22,6 @@ else
 {
 largest = c;
 }
+
 return (largest);
 }
