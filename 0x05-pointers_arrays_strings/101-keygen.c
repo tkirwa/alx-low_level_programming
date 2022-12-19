950d9fd1333e0af8b41e66635d167ef8de20bb11
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-19 14:22:02 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,37 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+    int r = 0, c = 0;
+    time_t t;
 
-#define PASSWORD_LEN 8
-
-/* Generates and returns a random password string.
-* The password string consists of upper and lower case letters,
-* and digits, and has a length of PASSWORD_LEN.
-*/
-char* generate_password(void) {
-static char password[PASSWORD_LEN + 1];
-int i;
-
-for (i = 0; i < PASSWORD_LEN; i++) {
-int r = rand() % 62;
-if (r < 26) {
-password[i] = 'a' + r;
-} else if (r < 52) {
-password[i] = 'A' + r - 26;
-} else {
-password[i] = '0' + r - 52;
-}
-}
-password[PASSWORD_LEN] = '\0';
-
-return password;
-}
-
-int main(void) {
-srand(time(NULL));
-
-char* password = generate_password();
-printf("Generated password: %s\n", password);
-
-return 0;
+    srand((unsigned int)time(&t));
+    while (c < 2772)
+    {
+        r = rand() % 128;
+        if ((c + r) > 2772)
+            break;
+        c = c + r;
+        printf("%c", r);
+    }
+    printf("%c\n", (2772 - c));
+    return (0);
 }
