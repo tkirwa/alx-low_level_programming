362ebf2daff088a05f8bd86593dccca1c95aedbf
Tonny Kirwa <tonnykirwa@yahoo.com>
2022-12-21 09:14:07 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int count = 0, count2 = 0;
+int count = 0, count2 = 0;
 
-    while (*(dest + count) != '\0')
-    {
-        count++;
-    }
+while (*(dest + count) != '\0')
+{
+count++;
+}
 
-    while (count2 >= 0)
-    {
-        *(dest + count) = *(src + count2);
-        if (*(src + count2) == '\0')
-            break;
-        count++;
-        count2++;
-    }
-    return (dest);
+while (count2 >= 0)
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
+count2++;
+}
+return (dest);
 }
