b80bc56de0e6a01705004adc8811be7e936b561c
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-11 05:47:23 -0800

    101-strtow.c - Write a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,64 +1,57 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
+ * strtow - splits a string to words
+ * @str: string to split
+ * Return: a point to an array of strings or NULL
  */
 char **strtow(char *str)
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+	char **arr_words = NULL;
+	int i, j = 0, wlen, slen, words = 0, sig = 0, pre_sig = 0;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
+	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	slen = strlen(str);
+	for (i = 0; i < slen; i++)
 	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
+		sig = (str[i] == 32 || str[i] == '\t') ? 0 : 1;
+		words = (pre_sig == 0 && sig == 1) ? words + 1 : words;
+		pre_sig = sig;
 	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
+	if (words == 0)
 		return (NULL);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
+	arr_words = malloc(words * sizeof(char *));
+	if (arr_words == NULL)
 	{
-		count = 0;
-		if (str[wordf] != ' ')
+		free(arr_words);
+		return (NULL);
+	}
+	words = 0;
+	for (i = 0; i < slen; i++)
+	{
+		sig = (str[i] == 32 || str[i] == 9) ? 0 : 1;
+		if (sig)
 		{
-			for (i = wordf ; str[i] != '\0'; i++)
+			for (j = 0; str[i + j] != 32 && str[i + j] != 9; j++)
+				;
+			wlen = j;
+			arr_words[words] = malloc(wlen * sizeof(char));
+			if (arr_words[words] == NULL)
 			{
-				if (str[i] == ' ')
-					break;
-				count++;
-			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == NULL)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
+				for (; words >= 0; words--)
+					free(arr_words[words]);
+				free(arr_words);
 				return (NULL);
 			}
-			for (m = 0; wordf < i; wordf++)
+			for (j = 0; j < wlen; j++)
 			{
-				p[j][m] = str[wordf];
-				m++;
+				arr_words[words][j] = str[i + j];
 			}
-			p[j][m] = '\0';
-			j++;
+			words++;
+			i += wlen - 1;
 		}
 	}
-	p[j] = NULL;
-	return (p);
+	return (arr_words);
 }
