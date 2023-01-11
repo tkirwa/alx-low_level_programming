dca37c1ad0f2ae57d287160f35b01cde0c76c6a0
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-11 05:51:53 -0800

    101-strtow.c - Write a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,57 +1,73 @@
-#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
- * strtow - splits a string to words
- * @str: string to split
- * Return: a point to an array of strings or NULL
+ * strtow - char
+ * @str: pointer to string params
+ * Return: char
  */
 char **strtow(char *str)
 {
-	char **arr_words = NULL;
-	int i, j = 0, wlen, slen, words = 0, sig = 0, pre_sig = 0;
-
-	if (str == NULL)
-		return (NULL);
-	slen = strlen(str);
-	for (i = 0; i < slen; i++)
-	{
-		sig = (str[i] == 32 || str[i] == '\t') ? 0 : 1;
-		words = (pre_sig == 0 && sig == 1) ? words + 1 : words;
-		pre_sig = sig;
-	}
-	if (words == 0)
-		return (NULL);
-	arr_words = malloc(words * sizeof(char *));
-	if (arr_words == NULL)
-	{
-		free(arr_words);
-		return (NULL);
-	}
-	words = 0;
-	for (i = 0; i < slen; i++)
-	{
-		sig = (str[i] == 32 || str[i] == 9) ? 0 : 1;
-		if (sig)
-		{
-			for (j = 0; str[i + j] != 32 && str[i + j] != 9; j++)
-				;
-			wlen = j;
-			arr_words[words] = malloc(wlen * sizeof(char));
-			if (arr_words[words] == NULL)
-			{
-				for (; words >= 0; words--)
-					free(arr_words[words]);
-				free(arr_words);
-				return (NULL);
-			}
-			for (j = 0; j < wlen; j++)
-			{
-				arr_words[words][j] = str[i + j];
-			}
-			words++;
-			i += wlen - 1;
-		}
-	}
-	return (arr_words);
+int i = 0, j = 0, k = 0;
+int len = 0, count = 0;
+char **f, *col;
+if (!str || !*str)
+{
+return (NULL);
+}
+while (*(str + i))
+{
+if (*(str + i) != ' ')
+{
+if (*(str + i + 1) == ' ' || *(str + i + 1) == 0)
+{
+count += 1;
+}
+}
+i++;
+}
+if (count == 0)
+{
+return (NULL);
+}
+count += 1;
+f = malloc(sizeof(char *) * count);
+if (!f)
+{
+return (NULL);
+}
+i = 0;
+while (*str)
+{
+while (*str == ' ' && *str)
+{
+str++;
+}
+len = 0;
+while (*(str + len) != ' ' && *(str + len))
+{
+len += 1;
+}
+len += 1;
+col = malloc(sizeof(char) * len);
+if (!col)
+{
+for (k = j - 1; k >= 0; k--)
+{
+free(f[k]);
+}
+free(f);
+return (NULL);
+}
+for (k = 0; k < (len - 1);  k++)
+{
+*(col + k) = *(str++);
+}
+*(col + k) = '\0';
+*(f + j) = col;
+if (j < (count - 1))
+{
+j++;
+}
+}
+*(f + j) = NULL;
+return (f);
 }
