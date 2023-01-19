175e74b2f349e76686aad8804b0db9385880e567
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-19 02:59:47 -0800

    1-array_iterator.c - Write a function that executes a function given as a parameter on each element of an array. -> Prototype: void array_iterator(int *array, size_t size, void (*action)(int)); -> where size is the size of the array -> and action is a pointer to the function you need to use

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
-/**                                                                             
+/**
 * array_iterator - executes a function given as a parameter on each element of\
-an array                                                                       
-* @array: array to iterate through                                             
-* @size: size of the array                                                     
-* @action: pointer to the function to use                                      
+an array
+* @array: array to iterate through
+* @size: size of the array
+* @action: pointer to the function to use
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
