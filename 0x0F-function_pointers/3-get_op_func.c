c7c37dc4facf1e6a492e458e0f4d387b826f7a7a
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-19 03:50:57 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h - Write a program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,23 +8,23 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-    int i;
+int i;
 
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 
-    i = 0;
-    while (ops[i].op != NULL)
-    {
-        if (*s == *(ops[i]).op)
-            return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+i = 0;
+while (ops[i].op != NULL)
+{
+if (*s == *(ops[i]).op)
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,32 +9,32 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*op_func)(int, int);
+int num1, num2;
+int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+if (op_func == NULL)
+{
+printf("Error\n");
+exit(99);
+}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
 
-    printf("%d\n", op_func(num1, num2));
+printf("%d\n", op_func(num1, num2));
 
-    return (0);
+return (0);
 }
