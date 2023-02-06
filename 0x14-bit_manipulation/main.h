c699e82f4f4f3a307708c27c0b8c11887ec02511
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-02-06 12:36:37 -0800

    1-print_binary.c - Write a function that prints the binary representation of a number.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,15 @@
  * there is one or more chars in the string b that is not 0 or 1
  * b is NULL
  */
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
 
 #endif
