b8ee5203b0f203fa01df37f2070846f66abc4291
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-16 09:36:45 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t nodes = 0;
+size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        nodes++;
-        h = h->next;
-    }
-    return (nodes);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
