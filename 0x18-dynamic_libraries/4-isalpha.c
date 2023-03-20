99eccf68be2028f9ffb10bb8daa063d6224e8a61
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-20 12:28:13 -0700

    libdynamic.so, main.h

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - tests whether a character is a
+ * alphabetic character.
+ * @c: character to test.
+ * Return: 1 if it is a alphabetic character...
+ * 0 if it is not a alphabetic character.
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
