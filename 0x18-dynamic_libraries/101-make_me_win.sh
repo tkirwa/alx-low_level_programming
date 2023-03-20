d9551dc58db408b837c95f2e33d1fdd9edb30f00
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-20 16:32:43 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-export LD_PRELOAD=./winning.so
+wget -P /tmp https://github.com/tkirwa/alx-low_level_programming/blob/main/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
