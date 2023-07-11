04ebffdcd04e29e7926ff10f796fa2025c0bf366
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-11 11:03:49 +0300

    0-linear.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+#include <stddef.h> /* Add this line to include size_t */
+
 /* Function prototype */
 int linear_search(int *array, size_t size, int value);
 
