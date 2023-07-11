2da56acf612fee26133fd1eb573714a2e346305f
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-11 12:24:34 +0300

    103-exponential.c

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,89 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - Searches for a value in a sorted array
+ *                      of integers using exponential search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the first index where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the array.
+ *              Uses powers of 2 as exponential ranges to search in the array.
+ *              Uses binary search to find the value within the range.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound = 1, low, high;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+
+	low = bound / 2;
+	high = (bound < size) ? bound : size - 1;
+
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+
+	return (binary_search(array, low, high, value));
+}
+
+/**
+ * binary_search - Searches for a value in a sorted array
+ *                 of integers using binary search.
+ * @array: A pointer to the first element of the array to search.
+ * @low: The starting index of the search range.
+ * @high: The ending index of the search range.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the index where the value is located.
+ *
+ * Description: Prints the subarray being searched in.
+ */
+int binary_search(int *array, size_t low, size_t high, int value)
+{
+	size_t mid;
+
+	while (low <= high)
+	{
+		print_subarray(array, low, high);
+		mid = (low + high) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
+	return (-1);
+}
+
+/**
+ * print_subarray - Prints the elements of a subarray.
+ * @array: A pointer to the first element of the array.
+ * @start: The starting index of the subarray.
+ * @end: The ending index of the subarray.
+ */
+void print_subarray(int *array, size_t start, size_t end)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%d", array[i]);
+		if (i < end)
+			printf(", ");
+	}
+	printf("\n");
+}
